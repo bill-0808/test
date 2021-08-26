634c548f6643d40af71011afdb54d07cd862e860
Khoa Nguyen <xuankhoa0808@gmail.com>
2021-08-26 18:11:44 +0700

    add comment

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -15,6 +15,7 @@
         {
             if (disposing && (components != null))
             {
+                //asdasdasdasdasd
                 components.Dispose();
             }
             base.Dispose(disposing);
